76e830071a618a1770e55d1a9ebf412d0976eb07
robemike <robemike1135@gmail.com>
2024-05-29 12:48:42 +0300

    completes code-along's tests

--- lib/car.py
+++ lib/car.py
@@ -4,3 +4,10 @@ class Car(Vehicle):
     def go(self):
         return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
     pass
+
+my_car = Car(24, 56)
+print(my_car.wheel_size)
+print(my_car.wheel_number)
+print(my_car.fill_up_tank())
+sound = my_car.go()
+print(sound)
